62e94f9544e4143ce00a7b01f452eaf64de7db51
lukhen <lukasz.hen@gmail.com>
2018-12-10 11:44:12 +0100

    Now the test methods are within a test class.

--- tests/test_find_price_in_memory_catalog.py
+++ tests/test_find_price_in_memory_catalog.py
@@ -1,10 +1,11 @@
 from tests.sale import Price
 
 
-def test_product_found():
-    found_price = Price.cents(1250)
-    catalog = InMemoryCatalog({'12345': found_price})
-    assert catalog.find_price('12345') == found_price
+class TestFindPriceInMemoryCatalog:
+    def test_product_found(self):
+        found_price = Price.cents(1250)
+        catalog = InMemoryCatalog({'12345': found_price})
+        assert catalog.find_price('12345') == found_price
 
 
 class InMemoryCatalog:
