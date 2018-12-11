2b7ac590d73ec011182966cb9e8dbcf33c6f29f6
lukhen <lukasz.hen@gmail.com>
2018-12-11 05:25:08 +0100

    InMemoryCatalog now really is a Catalog.
    Confirmed that InMemoryCatalog respects the semantics of Catalog.
    
    In fact this is a factory method
      - it returns a Catalog not InMemoryCatalog
      - does it matter in python ?
      - Catalog is preloaded with the 12345->foundPrice product
      - this test has no reference to the implementation of Catalog
      - but rather it describes what Catalog (interface) should do
    
      in case of product_not_found
       - empty dict is only an implementation detail
       - what we really want is a Catalog without a product 12345->found_price
       - it's just happens that the easiest way to do it is to provide an empty dict
       - test is very easy to read
         - if I try to find a barcode 12345 in a catalog without barcode 12345, I should get None

--- tests/test_find_price_in_memory_catalog.py
+++ tests/test_find_price_in_memory_catalog.py
@@ -4,9 +4,15 @@ from tests.sale import Price, InMemoryCatalog, Catalog
 class TestFindPriceInMemoryCatalog:
     def test_product_found(self):
         found_price = Price.cents(1250)
-        catalog = InMemoryCatalog({'12345': found_price})
+        catalog = self.catalogWith('12345', found_price)
         assert catalog.find_price('12345') == found_price
 
     def test_product_not_found(self):
-        catalog = InMemoryCatalog({})
+        catalog = self.catalog_without('12345')
         assert catalog.find_price('12345') is None
+
+    def catalog_without(self, barcode_to_avoid):
+        return InMemoryCatalog({})
+
+    def catalogWith(self, barcode, found_price) -> Catalog:
+        return InMemoryCatalog({barcode: found_price})
