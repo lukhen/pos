88dbb6172f0a37728f651153d16272b591ced8d1
lukhen <lukasz.hen@gmail.com>
2018-12-13 11:24:01 +0100

    We can display a product not found message, but only for the magic barcode.
    displaying price to the console
    2 ways:
      - hijack
      - introduce some abstraction to decouple
    
    Hijacking
     Substitute the system.out with our own output device (object) and grabbing
     data to examin what was sent to the output.
    
    Fortunately in python we can do it easily using pytest fixture capsys.

---
+++ tests/test_display_messages_to_console.py
@@ -0,0 +1,13 @@
+import io
+
+
+def test_product_not_found_message(capsys):
+    console_display = ConsoleDisplay()
+    console_display.display_product_not_found_message("91837248")
+    assert capsys.readouterr().out.splitlines() == [
+        'Product not found for 91837248']
+
+
+class ConsoleDisplay:
+    def display_product_not_found_message(self, barcode):
+        print('Product not found for 91837248')
