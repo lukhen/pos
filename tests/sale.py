faebf708649d1b9e2cf866c2e06460ee0c41f6d5
lukhen <lukasz.hen@gmail.com>
2018-12-19 06:08:41 +0100

    Imporved names.
    Placeholder is the place in the template.
    We pass a value to the function, which is used to fill the template .

--- tests/sale.py
+++ tests/sale.py
@@ -63,24 +63,24 @@ class ConsoleDisplay:
     SCANNING_ERROR_EMPTY_BARCODE_MESSAGE_FORMAT = 'Scanning error: empty barcode'
     PRICE_IN_DOLLARS_MESSAGE_FORMAT = "${:,.2f}"
 
-    def display_product_not_found_message(self, *placeholders):
+    def display_product_not_found_message(self, *placeholder_values):
         self.display(
-            self.PRODUCT_NOT_FOUND_MESSAGE_FORMAT, *placeholders)
+            self.PRODUCT_NOT_FOUND_MESSAGE_FORMAT, *placeholder_values)
 
-    def display_empty_barcode_message(self, *placeholders):
+    def display_empty_barcode_message(self, *placeholder_values):
         self.display(
-            self.SCANNING_ERROR_EMPTY_BARCODE_MESSAGE_FORMAT, *placeholders)
+            self.SCANNING_ERROR_EMPTY_BARCODE_MESSAGE_FORMAT, *placeholder_values)
 
-    def display_price_in_dollars(self, *placeholders):
+    def display_price_in_dollars(self, *placeholder_values):
         self.display(
-            self.PRICE_IN_DOLLARS_MESSAGE_FORMAT, *placeholders)
+            self.PRICE_IN_DOLLARS_MESSAGE_FORMAT, *placeholder_values)
 
-    def display(self, message_template, *placeholders):
+    def display(self, message_template, *placeholder_values):
         self._render(
-            self._format_message(message_template, *placeholders))
+            self._format_message(message_template, *placeholder_values))
 
     def _render(self, text):
         print(text)
 
-    def _format_message(self, message_template, *placeholders):
-        return str.format(message_template, *placeholders)
+    def _format_message(self, message_template, *placeholder_values):
+        return str.format(message_template, *placeholder_values)
