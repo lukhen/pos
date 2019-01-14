a8f2401ca7481640a981b4b59163cd2a9478947e
lukhen <lukasz.hen@gmail.com>
2019-01-14 06:04:06 +0100

    Now we can interpret up to one commans as a barcode.
    newlines ?
    should I separate stream of thext commands from processing stdin?
    seems too much of responsibility in one place:
     - interpreting text commands
     - consuming input and turning it into a string
    
    feels like there should be:
     - interpreter which intepretes the stream of lines
     - something that listenes to standard in and creates streams

--- tests/ui_tests/test_interpret_text_commands.py
+++ tests/ui_tests/test_interpret_text_commands.py
@@ -1,20 +1,38 @@
 from unittest.mock import Mock
 import io
 from abc import abstractmethod, ABC
+import pytest
 
 
 class TextCommandInterpreter:
+    def __init__(self, barcode_scanned_listener):
+        self._barcode_scanned_listener = barcode_scanned_listener
+
     def process(self, reader):
-        ...
+        barcode = reader.readline().strip()
+        print(barcode)
+        if barcode:
+            self._barcode_scanned_listener.onbarcode(barcode)
 
 
 class BarcodeScannedListener(ABC):
-    ...
+    def onbarcode(self, barcode):
+        ...
 
 
 def test_zero():
     barcode_scanned_listener = Mock(spec=BarcodeScannedListener)
 
-    TextCommandInterpreter().process(io.StringIO(""))
+    TextCommandInterpreter(barcode_scanned_listener).process(io.StringIO(""))
 
     assert barcode_scanned_listener.mock_calls == []
+
+
+def test_one():
+    barcode_scanned_listener = Mock(spec=BarcodeScannedListener)
+
+    TextCommandInterpreter(barcode_scanned_listener).process(
+        io.StringIO("::barcode::\n")
+    )
+
+    barcode_scanned_listener.onbarcode.assert_called_once_with("::barcode::")
